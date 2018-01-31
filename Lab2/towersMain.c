f6a54d9bd2fc289aa3d4c49a59145cd3d1ea8a1b
gabecasciano <gabecasciano@gmail.com>
2018-01-30 23:24:30 -0500

    second commit

--- Lab2/towersMain.c
+++ Lab2/towersMain.c
@@ -1,15 +1,26 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "towers.h"
 
 int main(int argc, char **argv)
 {
-    int n = 3;
-    int from = 1;
-    int dest = 2;
-    if (argc > 1) {
-        n = atoi(argv[1]);
-    }
-    towers(n, from, dest);
-    exit(0);
+	int n = 3;
+	int from = 1, dest = 2;
+	int temp;
+	if(argc == 2)
+		n = atoi(argv[1]);
+	else if(argc >= 3){
+		n = atoi(argv[1]);
+		from = atoi(argv[2]);
+		temp = atoi(argv[3]);
+		if(temp != from)
+			dest = temp;
+		else{
+			fprintf(stderr,"INCORRECT INPUT");
+			exit(0);
+		}
+	}
+	towers(n, from, dest);
+    	exit(0);
 }
 
