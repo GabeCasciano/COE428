ee15afe8cbc81eefd6724797f5979a412d77ef34
gabecasciano <gabecasciano@gmail.com>
2018-03-07 23:11:28 -0500

    Second commit, does not work

--- Lab4/README.txt
+++ Lab4/README.txt
@@ -1,1 +1,26 @@
 Ignore the CMake file, this is just so I can use a nicer IDE.
+
+The picture of the FSM is in the image titled "FSM_IMAGE.jpg"
+
+Does not work, I am struggling to get rid od Segmentation Faults.
+This lab is also not complete and I plan on submitting it late,
+
+node_t is the struct used to contain the state as well as the next
+states
+
+buildlist is used to initialize all of my node_t based variables.
+It will also intiialize some other global pointers
+
+updateCurrent updates the current state based on the users input
+
+change allows users to change the specified next state to any given
+state.
+
+print as it sounds, prints out the fsm
+
+garbage_collector is used to detect if an states are not reachable.
+This does not account for multiple states going missing at once.
+This is simply based on a checksum.
+
+ASCIIAdder is a method used to simply convert the pre-assigned state
+numbers into their corresponding ascii chatacters.
--- Lab4/simState.c
+++ Lab4/simState.c
@@ -2,57 +2,107 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#define MAX_SIZE 8
+#define ADD_FOR_CHAR 65;
+
 typedef struct node{
-    char state;
-    int pos; //pos will be used to identify which values are still accessible
-    struct node* zero;
-    struct node* one;
+    int state; //pos will be used to identify which values are still accessible
+    int zero;
+    int one;
 }node_t;
 
-void garbage(node_t* garbage_list, node_t* current){//TODO: figure out how to do me
-    //could do a checksum based on the new sum compared to the og sum we can determine which has been deleted
-    //create a list of garbage
-}
-void _delete(node_t* deleted_list, node_t* current){//TODO: copy me from that tutorial
-    //keep a list of deleted states to use for other methods
-}
-void change(int input_state, char node, node_t* current){//TODO: I should be a fun one
+node_t** state_machine;
+node_t* current;
+node_t** garbage;
+int** counter;
 
-}
-void print(node_t* current){//TODO: Copy me from that tutorial
+int ASCIIAdder(int val){ return val + ADD_FOR_CHAR; }
 
+void garbage_collector(){
+    int count[2] = {0,0};
+    int temp;
+    for(int i = 0; i < MAX_SIZE; i++){
+        count[0] += state_machine[i]->zero;
+        count[1] += state_machine[i]->one;
+    }
+    if(count[0] != *counter[0] && count[1] != *counter[0]){
+        temp = *counter[0] - count[0];
+        garbage[*counter[2]]-> state = temp;
+        *counter[2]++;
+    }
 }
-void updateCurrent(int input, node_t* current){//think about how to migrate the list //TODO: might have to use multiple lists
-
+void change(int input, int state_change){
+    if(state_change == 1)
+        current->one =  input;
+    else if(state_change == 0)
+        current->zero = input;
+    state_machine[current->state] = current;
+    garbage_collector();
 }
-void buildList(node_t* current){//TODO: do this the shitty way
+void print(){
+    for(int i = 0; i < MAX_SIZE; i++)
+        printf("%c %c %c \n", ASCIIAdder(state_machine[i]->state), ASCIIAdder(state_machine[i]->zero) , ASCIIAdder(state_machine[i]->one) );
+}
 
+void updateCurrent(int input){//think about how to migrate the list //TODO: might have to use multiple lists
+    if(input == 1) {
+        current = state_machine[current->one];
+    }
+    else if(input == 0){
+        current = state_machine[current->zero];
+    }
+    printf("%c \n", ASCIIAdder(current->state));
+}
+
+void buildList(){
+    *state_machine = calloc(sizeof(node_t), MAX_SIZE);
+    *garbage = calloc(sizeof(node_t), MAX_SIZE);
+    *counter = calloc(sizeof(int), 3);
+    int zeros[MAX_SIZE] = {4,2,2,0,1,6,5,4};
+    int ones[MAX_SIZE] = {6,3,7,5,3,1,2,0};
+    for(int i = 0; i < MAX_SIZE; i++) {
+        state_machine[i] = malloc(sizeof(node_t));
+        garbage[i] = malloc(sizeof(node_t));
+    }
+    for(int i = 0; i < MAX_SIZE; i++){
+        state_machine[i]->state = i;
+        state_machine[i]->zero = zeros[i];
+        state_machine[i]->one = ones[i];
+        *counter[0] += zeros[i];
+        *counter[1] += ones[i];
+    }
 }
 
 int main(int argc, char * argv[])
 {
     bool loop = true;
-    char* input;
+    char input[10];
+    buildList();
+    current = state_machine[7];
+    printf(ASCIIAdder(current->state));
     while(loop){
         printf("Enter your command:");
-        scanf('%c', input);
-        switch (*input){//use the switch statment to determine which method to call
+        scanf("%s", input);
+        switch (input[0]){//use the switch statment to determine which method to call
             case 'c': //change
+                change(input[4]-MAX_SIZE, input[2]-MAX_SIZE);
                 break;
             case 'g': //garbage
                 break;
             case 'd': //delete
                 break;
             case 'P': //print
+                print();
                 break;
             case '0': //update
             case '1':
+                updateCurrent(atoi(input));
                 break;
             case 'e': //exit
                 loop = false;
                 break;
             default:
-                printf("Please enter a valid command");
+                printf("Please enter a valid  \n");
                 break;
         }
 
